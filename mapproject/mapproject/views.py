d46be598dc7ef92e3cac7c6a99f29a75cab86ff3
Diablo <thediabloxtreme666@gmail.com>
2025-11-21 10:57:11 +0530

    Changed from add to index

--- mapproject/mapproject/views.py
+++ mapproject/mapproject/views.py
@@ -2,15 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-
-def index(request):
-    return render(request, 'index.html')
-
 @csrf_exempt
-def add(request):
+def index(request):
     if request.method == "POST":
+        print(request.POST)
         name = request.POST.get("name")
         place_type = request.POST.get("type")
+        lat = request.POST.get("latitude")
+        lon = request.POST.get("longitude")
         print(f"Name: {name}, Type: {place_type}")
-        return HttpResponse("Form submitted!")
-    return HttpResponse("Invalid request")
+        # return HttpResponse(f"{name}, {place_type}, {lat}, {lon}")
+    else:
+        return render(request, 'index.html')
+
+
